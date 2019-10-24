2449b7c8c17f4937f4bf236815dcba8fa1c10da6
Wachrip <svardmitriy@gmail.com>
2019-10-24 16:59:50 +0600

    refactored User component with useEffect

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,28 +1,20 @@
-import React, { Component, Fragment } from 'react'
+import React, { useEffect, Fragment } from 'react'
 import Spinner from '../layout/Spinner'
 import Repos from '../Repos/Repos'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login)
-    this.props.getUserRepos(this.props.match.params.login)
-  }
+const User = ({user, loading, getUser, getUserRepos, repos, match}) => {
+  useEffect(() => {
+    getUser(match.params.login)
+    getUserRepos(match.params.login)
+    // eslint-disable-next-line
+  }, [getUser, getUserRepos, match.params.login])
 
-  static propTypes = {
-    loading: PropTypes.bool.isRequired,
-    user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
-    repos: PropTypes.array.isRequired,
-  }
+    const { name, avatar_url, location, bio, blog, login, html_url, followers, following, public_repos, public_gists, hireable, company} = user
 
-  render() {
-    const { name, avatar_url, location, bio, blog, login, html_url, followers, following, public_repos, public_gists, hireable, company} = this.props.user
-
-    const { loading, repos } = this.props
     if(loading) return <Spinner />
+
     return (     
       <Fragment>
         <Link to ='/' className='btn btn-light'>
@@ -85,7 +77,14 @@ class User extends Component {
         <Repos repos={repos}/>
       </Fragment>
     )
-  }
+}
+
+User.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  user: PropTypes.object.isRequired,
+  getUser: PropTypes.func.isRequired,
+  getUserRepos: PropTypes.func.isRequired,
+  repos: PropTypes.array.isRequired,
 }
 
 export default User
